0bba68a0ee2da6e62131150c1e7ff34ac134e654
Habbatul <gamernolifeneet@gmail.com>
2024-05-24 10:34:46 +0700

    add deployment configuration with vite

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+
+export default defineConfig({
+  assetsInclude: ['**/*.glb'],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+            src: 'gltf/*',
+            dest: 'gltf'
+          },
+          {
+            src: 'audio/*',
+            dest: 'audio'
+          },
+          {
+            src: 'hdri/*',
+            dest: 'hdri'
+          }
+      ]
+    })
+  ],
+  base : '/3D-Nostalgia/'
+});
